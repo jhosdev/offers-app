6854410e5fc8c15871d2d93206d743273f42b943
avi-2-avi <avicon95@protonmail.com>
2023-05-06 20:08:07 -0500

    chore: home card

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { OfferService } from 'src/app/features/offers/services/offer.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  constructor(private offerService: OfferService) {}
+  totalOffers!: number;
 
+  ngOnInit(): void {
+    this.getOffers();
+  }
+
+  getOffers(): void {
+    this.offerService.getOffers().subscribe((response) => {
+      this.totalOffers = response.length;
+    });
+  }
 }
